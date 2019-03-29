c01cf667fb26bbb9c24f0ed37c86aaedf4b798ee
XiaoHsiang <48340410+XiaoHsiang@users.noreply.github.com>
2019-03-29 10:33:22 +0800

    Add files via upload

---
+++ Basic-homework10.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	char name[100];  //建立char陣列 
+	int i=0;         //建立int變數=0
+	char option;
+	while(true){
+		printf("輸入字串\n");
+		scanf("%s",&name);
+		while(i<100&&name[i]>=65){
+			if(name[i]>=65&&name[i]<=90){
+				name[i]+=32;
+				printf("%c",name[i]);
+			}
+			else if(name[i]>=97&&name[i]<=122){
+				name[i]-=32;
+				printf("%c",name[i]);
+			}
+			i++;
+		}
+		printf("\n");
+		i=0;
+	} 
+	
+	
+} 
